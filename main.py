ae262fec0b63288ae4425ee3523feb1c665e0a8c
DG <dalibor1910@gmail.com>
2024-01-23 20:44:20 +0100

    LinkedList

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def vloz(self, prvok):
+        if self.head is None:
+            self.head = prvok
+        else:
+            aktualny = self.head
+            while aktualny.next is not None:
+                aktualny = aktualny.next
+            aktualny.next = prvok
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def vypis(self):
+        aktualny = self.head
+        print(aktualny.data)
+        while aktualny.next is not None:
+            aktualny = aktualny.next
+            print(aktualny.data)
+
+    def vloz_na_zaciatok(self, prvok):
+        if self.head is None:
+
+            self.head = prvok
+        else:
+            predtym_prvy = self.head
+            self.head = prvok
+            prvok.next = predtym_prvy
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Prvok:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+mojLinked = LinkedList()
+prvok1 = Prvok("Milan")
+mojLinked.vloz_na_zaciatok(prvok1)
+prvok2 = Prvok("Jozo")
+mojLinked.vloz_na_zaciatok(prvok2)
+prvok3 = Prvok("Fero")
+mojLinked.vloz_na_zaciatok(prvok3)
+mojLinked.vypis()
+print("test")
